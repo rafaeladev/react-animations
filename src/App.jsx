5cc31f49c3d770bb6e77c05f211c8c6100e9a24d
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-09-17 16:22:07 +0200

    project ended

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Layout from './components/Layout'
 import Home from './components/Home/index.jsx'
+import About from './components/About/index.jsx'
+import Contact from './components/Contact/index.jsx'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,8 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters/index.jsx'
+import Logo from './Logo/index.jsx'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -31,34 +33,38 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
 
-        <h2>Frontend Developer / JavaScript Expert / Designer </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+          <h2>Frontend Developer / JavaScript Expert / Designer </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
