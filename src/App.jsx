fc6c23db3bb7b33de078c7bb076f2e28815d1fde
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-09-17 12:15:43 +0200

    new animations folio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.scss'
+import Layout from './components/Layout'
+import Home from './components/Home/index.jsx'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
